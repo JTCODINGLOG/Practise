f72fd05575e7abb82a2b159fc2fc7bdd7f54fe99
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-05-08 12:44:19 +0000

    Mon, May 8, 2023, 2:44 PM +02:00

--- hello.c
+++ hello.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include <cs50.h>
 
 int main(void)
 {
-    printf("hello, world\n");
+    string answer = get_string("What's your name? ");
+    printf("hello, answer\n");
 }
+
+
+
+
+
