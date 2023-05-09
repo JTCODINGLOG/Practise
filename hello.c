282ada21b06ff75782e6dd3a8748039f2c007f7d
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-05-09 07:41:24 +0000

    Tue, May 9, 2023, 9:41 AM +02:00

--- hello.c
+++ hello.c
@@ -6,6 +6,14 @@ int main(void)
     string answer = get_string("What's your name? ");
     printf("hello, %s!\n", answer);
 }
+if (x < y)
+{
+    printf("x is less than y\n")
+}
+else
+{
+    printf("x is not less than y\n")
+}
 
 
 
