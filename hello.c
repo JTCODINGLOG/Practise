8a792062248aaf446ac8daf6a0511ca8d59038d2
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-05-09 07:45:22 +0000

    Tue, May 9, 2023, 9:45 AM +02:00

--- hello.c
+++ hello.c
@@ -8,15 +8,15 @@ int main(void)
 }
 if (x < y)
 {
-    printf("x is less than y\n")
+    printf("x is less than y\n");
 }
 else if (x > y)
 {
-    printf("x is not less than y\n")
+    printf("x is not less than y\n");
 }
 else
 {
-    printf("x is equal to y\n")
+    printf("x is equal to y\n");
 }
 
 
