3002f0c6a0316fe53e4d206da3d2cfa0de36061d
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-05-09 12:39:41 +0000

    Tue, May 9, 2023, 2:39 PM +02:00

--- mario.c
+++ mario.c
@@ -5,6 +5,9 @@ int main(void)
     for (int i = 0; i < 3; i++)
     {
         for (int i = 0; i <3; i++)
-        {printf (#)}
+        {
+            printf ("#");
+        }
+        printf ("\n");
     }
 }
