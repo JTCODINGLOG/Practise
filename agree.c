5d345d7fa9d75736cd95c07bda78d7804b158c87
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-05-09 08:08:31 +0000

    Tue, May 9, 2023, 10:08 AM +02:00

--- agree.c
+++ agree.c
@@ -4,5 +4,15 @@
 int main(void)
 {
     char c = get_char("Do you agree?");
+
+    if (c == 'y')
+    {
+        printf("Agreed.\n");
+
+    }
+    else if (c == 'n')
+    {
+        printf("Not agreed.\n");
+    }
 }
 
