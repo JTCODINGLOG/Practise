d5ee042d8ec36a3f26ed5e9af4e183d05cc273f4
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-05-10 02:16:08 +0000

    Wed, May 10, 2023, 4:16 AM +02:00

--- mario.c
+++ mario.c
@@ -4,20 +4,13 @@
 int main(void)
 {
     //Get size of grid
-    int n;
-    do
-    {
-        n = get_int("Size: ");
-    }
-    while (n < 1);
+    int n = get_size();
 
     //Prints grid of bricks
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            printf ("#");
-        }
-        printf ("\n");
-    }
+    print_grid(n);
+}
+
+int get_size(void)
+{
+    int n
 }
