db22dae0aca925a3c86b3809ea47788d3c4bc488
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-05-10 06:43:24 +0000

    Wed, May 10, 2023, 8:43 AM +02:00

--- population.c
+++ population.c
@@ -6,12 +6,10 @@ int main (void)
     int nstart = get_int ("What is the start size?: ");
     int nend = get_int ("What is the end size?: ");
 
-
-    for (int y = 1; nstart <= nend ; y++)
+    for (int y = 1; nstart > nend ; y++)
     {
         nstart = nstart *((13/12)^y);
-        printf ("The number of years is %i", y );
-
+        printf ("The number of years is %i\n", y );
     }
 
 }
