f77d8ddac9f8d0f4f92dfcc5e61eb4608ef6a5d5
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-05-10 11:40:01 +0000

    Wed, May 10, 2023, 1:40 PM +02:00

---
+++ half.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <cs50.h>
+
+float bill = get_float ("Bill before tax and tip: ");
+float tax = get_float ("Sale Tax Percent: ");
+float tip  = get_float ("Tip percent: ");
+
