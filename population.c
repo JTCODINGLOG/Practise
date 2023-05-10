7ecdae86a3191c205635df9b1a43db2057ccda7d
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-05-10 07:00:39 +0000

    Wed, May 10, 2023, 9:00 AM +02:00

--- population.c
+++ population.c
@@ -3,12 +3,13 @@
 
 int main (void)
 {
-    int nstart = get_int ("What is the start size?: ");
-    int nend = get_int ("What is the end size?: ");
+
 
     do
     {
         {
+            int nstart = get_int ("What is the start size?: ");
+            int nend = get_int ("What is the end size?: ");
             int y = 0;
             y = y + 1;
             nstart = nstart *((13/12)^y);
