c93214da79a0d37f1126e5b6d952554b3b7a5c4f
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-05-10 12:05:46 +0000

    Wed, May 10, 2023, 2:05 PM +02:00

--- half.c
+++ half.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <cs50.h>
 
-float bill = get_float ("Bill before tax and tip: ");
-float tax = get_float ("Sale Tax Percent: ");
-float tip  = get_float ("Tip percent: ");
+
 
 int main(void)
 {
 
 }
+
+float bill = get_float ("Bill before tax and tip: ");
+float tax = get_float ("Sale Tax Percent: ");
+float tip  = get_float ("Tip percent: ");
 
 float share(bill, tax, tip)
 {
