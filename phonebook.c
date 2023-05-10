ca729246e4715b7a8506d108ecb973b9bca48d9a
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-05-10 04:37:57 +0000

    Wed, May 10, 2023, 6:37 AM +02:00

--- phonebook.c
+++ phonebook.c
@@ -3,6 +3,8 @@
 
 int main (void)
 {
-    string answer = get_string ("What's your name?: ");
-    printf ("hello, %s", answer);
+    string first_answer = get_string ("What's your name?: ");
+    int second_answer = get_int ("What's your age?: ");
+    int third_answer = get_int ("What's your phone?: ");
+    printf ("%s\n%i\n%i\n", first_answer, second_answer, third_answer);
 }
