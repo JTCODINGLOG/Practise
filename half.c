4495aa994250a1b9f7a34f81eaf0d91875861866
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-05-10 12:12:58 +0000

    Wed, May 10, 2023, 2:12 PM +02:00

--- half.c
+++ half.c
@@ -6,15 +6,12 @@
 int main(void)
 {
 
-float bill = get_float ("Bill before tax and tip: ");
-float tax = get_float ("Sale Tax Percent: ");
-float tip  = get_float ("Tip percent: ");
+    float bill = get_float ("Bill before tax and tip: ");
+    float tax = get_float ("Sale Tax Percent: ");
+    float tip  = get_float ("Tip percent: ");
 
-
-bill = bill * (1+ (tax / 100) );
-bill = bill * (1+ (tip /100) );
-bill = bill / 2;
-
-printf ("You will owe $ %.2f each!\n", bill);
+    bill = bill * (1+ (tax / 100) )*(1+ (tip /100) )/2;
+    
+    printf ("You will owe $ %.2f each!\n", bill);
 
 }
