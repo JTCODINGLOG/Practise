a613f9f3cb11c4896ac4ffc28f6e455ec5746859
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-05-13 08:56:53 +0000

    Sat, May 13, 2023, 10:56 AM +02:00

--- credit/credit.c
+++ credit/credit.c
@@ -6,10 +6,10 @@ int main(void)
     //long number = get_long ("Number: ");
     long number;
 
-    number = 4003600000000014 %100;
+    number = 4003600000000014 %10;
     printf (" %li \n", number);
 
-    number = 4003600000000014 %10000;
+    number = 4003600000000014 %100;
     printf (" %li \n", number);
 
     number = 4003600000000014 %1000000;
