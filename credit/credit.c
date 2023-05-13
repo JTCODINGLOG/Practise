c0269f806baf2f4cda6e338c20d5d2459d8a293e
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-05-13 09:06:01 +0000

    Sat, May 13, 2023, 11:06 AM +02:00

--- credit/credit.c
+++ credit/credit.c
@@ -4,28 +4,27 @@
 int main(void)
 {
     //long number = get_long ("Number: ");
+    long number1;
+    long number2;
+    long number12;
+    long number13;
+
+    number1 = 4003600000000014 %10;
+
+    number2 = 4003600000000014 %100;
+    printf ("Numero 2 es %li \n", (number2-number1)10;
+
+
+
+    number12 = 4003600000000014 %100000000000;
+
+    number13 = 4003600000000014 %1000000000000;
+    printf ("Numero 13 es %li \n", (number13-number12)100000000;
+
     long number;
 
-    number = 4003600000000014 %10;
-    printf (" %li \n", number);
-
-    number = 4003600000000014 %100;
-    printf (" %li \n", number);
-
-    number = 4003600000000014 %1000000;
-    printf (" %li \n", number);
-
-    number = 4003600000000014 %100000000;
-    printf (" %li \n", number);
-
-    number12 = 4003600000000014 %10000000000;
-    printf (" %li \n", number);
-
-    number = 4003600000000014 %100000000000;
-    printf (" %li \n", number);
-
-    number = 4003600000000014 %100000000000000;
-    printf (" %li \n", number);
+    number = 4003600000000014 / 10000000000000;
+    printf (" %li", number);
 
 
     //American Express 15 digits, starts with 34 or 37
