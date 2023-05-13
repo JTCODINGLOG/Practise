2f39450d3b79c06dbcd7c541f45cf65ac42534d6
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-05-13 08:12:29 +0000

    Sat, May 13, 2023, 10:12 AM +02:00

--- credit/credit.c
+++ credit/credit.c
@@ -3,6 +3,11 @@
 
 int main(void)
 {
-    int number = get_int ("number: ");
-    printf
+    int number = get_int ("Number: ");
+
+    //American Express 15 digits, starts with 34 or 37
+    //MasterCard 16 digits, starts with 51,52,53,54,55
+    //Visa 13 or 16, starts with 4
+
+    //Check numbers 
 }
