07dff0aabab77522755a9f529c91d36e6dd20c9a
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-05-13 05:40:25 +0000

    Sat, May 13, 2023, 7:40 AM +02:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -11,9 +11,9 @@ int main(void)
     }
     while (height<1 || height>8);
 
-    for (int i = 0; height )
+    for (int i = 0; i<height ; i++)
     {
-        printf ("#")
+        printf ("#");
     }
 
 }
