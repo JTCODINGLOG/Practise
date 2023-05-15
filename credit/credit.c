bf2392d05d7137760e7bfaea4ac465dea43ae5dc
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-05-15 04:23:26 +0000

    Mon, May 15, 2023, 6:23 AM +02:00

--- credit/credit.c
+++ credit/credit.c
@@ -77,28 +77,4 @@ int main(void)
     }
     else
         printf ("INVALID \n");
-
-
-
-
-
-
-
-
-    //number extraction
-    //int n1;
-    //n1 = number %10;
-    //int n2;
-    //n2 = number %100/10;
-
-
-    //American Express 15 digits, starts with 34 or 37
-    //MasterCard 16 digits, starts with 51,52,53,54,55
-    //Visa 13 or 16, starts with 4
-
-    //Check numbers
-
-    //% 10 to get rest and figure out digit
-
-    //4003600000000014
 }
