97ba09f21c4781ca2a09178257de2b432405f8f5
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-05-15 01:00:26 +0000

    Mon, May 15, 2023, 3:00 AM +02:00

--- number.c
+++ number.c
@@ -10,15 +10,16 @@ int main(void)
     }
     while (3999999999999 > number);
 
-    int n1;
-    n1 = number %10;
-    int n2;
-    n2 = number %100/10;
-    int n3;
-    n3 = number %1000/100;
+
+    //int n1;
+    //n1 = number %10;
+    //int n2;
+    //n2 = number %100/10;
+    //int n3;
+    //n3 = number %1000/100;
 
     int n;
-    for (int i = 1; n==number ; i*10 )
+    for (int i = 1; n==number ; i=i*10 )
     {
         n = number %(i*10) / i;
         printf ("%i", n);
