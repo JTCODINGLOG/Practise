417e523464219c1872fa1bf06b4a55a80f6449ba
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-05-15 04:09:02 +0000

    Mon, May 15, 2023, 6:09 AM +02:00

--- credit/credit.c
+++ credit/credit.c
@@ -55,12 +55,12 @@ int main(void)
     }
 
     //find first 2 digits
-    int start = number;
+    long start = number;
     do
     {
-    start = start / 10;
+        start = start / 10;
     }
-    while (start > 100)
+    while (start > 100);
 
     //deciding the type of card
     if ((start / 10 == 5) && (0 < start % 10 && start % 10 < 6 ) && (i == 16))
