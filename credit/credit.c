47a83b89a83fbd6477c9a1ef8ac1830c7720c915
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-05-15 03:15:55 +0000

    Mon, May 15, 2023, 5:15 AM +02:00

--- credit/credit.c
+++ credit/credit.c
@@ -23,7 +23,29 @@ int main(void)
     }
 
     //checksum
-    
+    long n = number;
+    int n1;
+    int n2;
+    int sum1 = 0;
+    int sum2 = 0;
+    int d1 = 0;
+    int d2 = 0;
+    do
+    {
+    //sum1
+    n1 = n % 10;
+    sum1 = sum1 + n1;
+    n = n/10;
+
+    //sum2
+    n2 = n % 10;
+    d1 = n2 % 10;
+    d2 = n2 / 10;
+    sum2 = sum2 + d1 +d2;
+
+
+    }
+
 
 
 
