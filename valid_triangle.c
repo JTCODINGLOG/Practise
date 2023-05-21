09cd5447d6ff71eae7e4c0790628d54f43d3f3cb
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-05-21 03:50:49 +0000

    Sun, May 21, 2023, 5:50 AM +02:00

--- valid_triangle.c
+++ valid_triangle.c
@@ -5,13 +5,30 @@ bool valid_triangle (float a, float b, float c);
 
 int main(void)
 {
+    float side_a = get_float ("Insert side a: ");
+    float side_b = get_float ("Insert side b: ");
+    float side_c = get_float ("Insert side c: ");
+
+    bool valid_triangle (float a, float b, float c);
 
 }
 
-bool valid_triangle
+bool valid_triangle (float a, float b, float c)
 {
-    if(a+b>ck)
-    return True
+    if (a>0 && b>0 && c>0)
+    {
+        if (a+b>c || a+c>b || b+c>a)
+        {
+            return True;
+        }
+        else
+        {
+            return False;
+        }
+    }
     else
-    return False
+    {
+        return False;
+    }
+
 }
