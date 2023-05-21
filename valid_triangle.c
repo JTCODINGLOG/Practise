2b75e481f0221f11d9ff332a0796145feb21fb15
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-05-21 03:13:30 +0000

    Sun, May 21, 2023, 5:13 AM +02:00

---
+++ valid_triangle.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <cs50.h>
+
+bool valid_triangle (float a, float b, float c);
+
+int main(void)
+{
+
+}
+
+bool valid_triangle
+{
+    if(a+b>c)
+    return True
+    else
+    return False
+}
