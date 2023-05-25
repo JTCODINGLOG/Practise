6d9c3ca4fc3f134774451a79ec752ba658fd0c2f
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-05-25 11:12:54 +0000

    Thu, May 25, 2023, 1:12 PM +02:00

---
+++ alphabet.c
@@ -0,0 +1,13 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <ctype.h>
+
+int main(void)
+{
+    string low;
+    do
+    {
+        low= get_string ("insert lowercase string")
+    }
+    while 
+}
