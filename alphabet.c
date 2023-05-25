ff88bfc260c83a8db65412d59c43681ddf6d6aab
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-05-25 11:24:26 +0000

    Thu, May 25, 2023, 1:24 PM +02:00

--- alphabet.c
+++ alphabet.c
@@ -1,16 +1,17 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 int main(void)
 {
     string low = get_string ("insert string: ");
 
-    int n =
+    int n = strlen (low);
 
     for(int i = 0; i < n; i++)
     {
-        if
+        if (low[0]
     }
-    
+
 }
