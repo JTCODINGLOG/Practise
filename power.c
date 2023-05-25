7ec6a3234cc91b91561ad4c4a9fc0c5bdc012169
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-05-25 10:22:00 +0000

    Thu, May 25, 2023, 12:22 PM +02:00

--- power.c
+++ power.c
@@ -3,7 +3,12 @@
 
 int main(void)
 {
-    int n = get_int ("Size: ");
+    int n;
+    do
+    {
+        n = get_int ("Size: ");
+    }
+    while (n < 1);
 
     int powers[n];
     for (int i = 1, j = 0; j < n; i = i*2, j++)
