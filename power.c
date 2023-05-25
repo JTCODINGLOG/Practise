287394e4f91235cb61a52615b32e2c8a60ccd892
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-05-25 10:37:47 +0000

    Thu, May 25, 2023, 12:37 PM +02:00

--- power.c
+++ power.c
@@ -10,12 +10,15 @@ int main(void)
     }
     while (n < 1);
 
-    power[0] = 1
-    int powers[n];
-    for (int i = 1; i =< n; i++)
+    int power[n];
+
+    power[0] = 1;
+    printf("%i", power[0]);
+
+    for (int i = 1; i < n; i++)
     {
         power[i] = 2 * power[i - 1];
 
-        printf( "%i\n", power [i] );
+        printf( "%i\n", power[i] );
     }
 }
