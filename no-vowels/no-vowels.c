7780cd5a1634f9ce5f9395903da5f54a14a0d7d6
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-05-27 06:22:03 +0000

    Sat, May 27, 2023, 8:22 AM +02:00

--- no-vowels/no-vowels.c
+++ no-vowels/no-vowels.c
@@ -31,21 +31,21 @@ string replace (string argv[1])
          {
             argv[1][i] = 6;
          }
-         if else(argv[1][i] == 'e')
+         if (argv[1][i] == 'e')
          {
             argv[1][i] = 3;
          }
-         if else(argv[1][i] == 'i')
+         if (argv[1][i] == 'i')
          {
             argv[1][i] = 1;
          }
-         if else(argv[1][i] == 'o')
+         if (argv[1][i] == 'o')
          {
             argv[1][i] = 0;
          }
          else
          {
-            return argv[1][i]
+            return 0;
          }
     }
 
