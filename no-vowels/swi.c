c10af85bd65bee018fee0d2f9b1e5939c092dd00
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-05-27 07:22:23 +0000

    Sat, May 27, 2023, 9:22 AM +02:00

---
+++ no-vowels/swi.c
@@ -0,0 +1,52 @@
+// Write a function to replace vowels with numbers
+// Get practice with strings
+// Get practice with command line
+// Get practice with switch
+
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+string replace(string N);
+
+int main(int argc, string argv[])
+{
+    if (argc == 2)
+    {
+        printf ("%s\n", replace(argv[1]));
+    }
+    else
+    {
+        printf ("please, insert just one command line argument\n");
+        return 1;
+    }
+}
+
+string replace (string N)
+{
+    int n = strlen(N);
+
+    for(int i = 0; i < n; i)
+    {
+         switch 
+
+         if (N[i] == 'a')
+         {
+            N[i] = 6;
+         }
+         else if (N[i] == 'e')
+         {
+            N[i] = 3;
+         }
+         else if (N[i] == 'i')
+         {
+            N[i] = 1;
+         }
+         else if (N[i] == 'o')
+         {
+            N[i] = 0;
+         }
+         return 0;
+    }
+    return N;
+}
