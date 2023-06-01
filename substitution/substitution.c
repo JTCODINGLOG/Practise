897e4a1959cbe732fd2cc9ddf1edc15aef706968
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-06-01 02:22:51 +0000

    Thu, Jun 1, 2023, 4:22 AM +02:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,7 +1,16 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <>
 
-int main(int argc, string argv[]) 
+int main(int argc, string argv[])
 {
+    if (string argv[1] == 26)
+    {
+        string plaintext = get_string
+    }
 
+    else
+    {
+        printf ("Key must contain 26 characters\n");
+    }
 }
