ce0be4e25fb38f0a791ac6a7b9837ff3f4f25b29
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-06-01 01:58:42 +0000

    Thu, Jun 1, 2023, 3:58 AM +02:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -27,7 +27,7 @@ int compute_score(string word)
     for (int i = 0, l = strlen(word); i < l; i++ )
         {
             word[i] = toupper (word[i]);
-            
+
             if (65 > int word[i] || int word[i] > 90)
             {
                 sum += 0;
