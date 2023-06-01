6d8fceb5bb55d722229f2b20cdcbd7e60abc70af
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-06-01 02:02:47 +0000

    Thu, Jun 1, 2023, 4:02 AM +02:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -19,6 +19,18 @@ int main(void)
     int score2 = compute_score(word2);
 
     // TODO: Print the winner
+    if (score1 > score2)
+    {
+        printf ("Player 1 wins!/n");
+    }
+    else if (score2 > score1)
+    {
+        printf ("Player 2 wins!/n");
+    }
+    else
+    {
+        printf ("Tie!/n")
+    }
 }
 
 int compute_score(string word)
@@ -28,13 +40,13 @@ int compute_score(string word)
         {
             word[i] = toupper (word[i]);
 
-            if (65 > int word[i] || int word[i] > 90)
+            if (65 > word[i] || word[i] > 90)
             {
                 sum += 0;
             }
             else
             {
-                int n = int word[i]-65;
+                int n = word[i]-65;
                 sum += POINTS[n];
             }
         }
