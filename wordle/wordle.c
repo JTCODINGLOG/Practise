328b91ecd0c9e1121f4f00adc0b03f8f24495c94
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-06-08 11:51:32 +0000

    Thu, Jun 8, 2023, 1:51 PM +02:00

--- wordle/wordle.c
+++ wordle/wordle.c
@@ -37,7 +37,7 @@ int main(int argc, string argv[])
 
     // ensure argv[1] is either 5, 6, 7, or 8 and store that value in wordsize instead
     // TODO #2
-    wordsize = argv[1];
+    wordsize = argv[1][0];
 
     if (4 > wordsize || wordsize > 8)
     wordsize = argv[1];
