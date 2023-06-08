61a90748013b2aad497ed0a964390a64a359837c
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-06-08 03:39:12 +0000

    Thu, Jun 8, 2023, 5:39 AM +02:00

--- wordle/wordle.c
+++ wordle/wordle.c
@@ -38,9 +38,10 @@ int main(int argc, string argv[])
     // ensure argv[1] is either 5, 6, 7, or 8 and store that value in wordsize instead
     // TODO #2
 
-    if (3 < argv[1] < 9)
+    if (4 > argv[1] > 8)
     {
-        
+        printf ("Error: wordsize must be either 5, 6, 7, or 8\n");
+        return 1;
     }
 
     // open correct file, each file has exactly LISTSIZE words
