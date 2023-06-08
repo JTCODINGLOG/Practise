72fab31a912c93a16b048936d4bcf6cb9502d9e2
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-06-08 12:05:35 +0000

    Thu, Jun 8, 2023, 2:05 PM +02:00

--- wordle/wordle.c
+++ wordle/wordle.c
@@ -38,7 +38,7 @@ int main(int argc, string argv[])
     // ensure argv[1] is either 5, 6, 7, or 8 and store that value in wordsize instead
     // TODO #2
     wordsize = argv[1][0];
-
+    printf ("%i", wordsize);
     if (wordsize < 5 || wordsize > 8)
     {
         printf ("Error: wordsize must be either 5, 6, 7, or 8\n");
