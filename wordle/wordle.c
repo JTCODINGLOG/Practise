1fb3d31b120f3655b9b3d769fc4c34820f188321
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-06-15 12:04:29 +0000

    Thu, Jun 15, 2023, 2:04 PM +02:00

--- wordle/wordle.c
+++ wordle/wordle.c
@@ -116,9 +116,12 @@ string get_guess(int wordsize)
     // ensure users actually provide a guess that is the correct length
     // TODO #3
 
-    for ()
-    {}
-    guess = int_string ("Input a 5-letter word: ");
+    int i = 0;
+    do
+    {
+        guess = int_string ("Input a 5-letter word: ");
+    }
+    while (i != strlent(wordsize))
 
 
     return guess;
