f13e92f58f8dd56517fafbc8a43be9ca7e10bdf6
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-06-18 13:08:45 +0000

    Sun, Jun 18, 2023, 3:08 PM +02:00

--- wordle/wordle.c
+++ wordle/wordle.c
@@ -110,7 +110,7 @@ int main(int argc, string argv[])
     // Print the game's result
     // TODO #7
 
-    if ( == choice)
+    if (won == true)
     printf("You win!");
     else
     printf("You lose...");
