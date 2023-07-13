efb87253aa04aa8335afb7c95db0dd99022d5b3c
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-07-13 01:53:37 +0000

    Thu, Jul 13, 2023, 3:53 AM +02:00

--- Collatz/collatz.c
+++ Collatz/collatz.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <cs50>
 
-int collatz (n)
+int collatz (int n)
 {
     if (n == 1)
-    return 0
+    return 0;
     else if (n % 2 == 0)
+    collatz(n/2);
     else
+    collatz (3*n + 1);
 }
 
 int main(void)
