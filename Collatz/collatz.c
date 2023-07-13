15f9ff5e8491822075f9b9efa6a8904b38d38afe
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-07-13 01:51:29 +0000

    Thu, Jul 13, 2023, 3:51 AM +02:00

--- Collatz/collatz.c
+++ Collatz/collatz.c
@@ -1,5 +1,15 @@
-collatz (n)
+#include <stdio.h>
+#include <cs50>
 
-if n ==1
+int collatz (n)
+{
+    if (n == 1)
     return 0
-if n
+    else if (n % 2 == 0)
+    else
+}
+
+int main(void)
+{
+
+}
