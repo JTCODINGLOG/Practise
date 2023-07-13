600fcee27741e3005eea33cacb8116cc8f7e8503
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-07-13 01:59:12 +0000

    Thu, Jul 13, 2023, 3:59 AM +02:00

--- Collatz/collatz.c
+++ Collatz/collatz.c
@@ -6,20 +6,20 @@ int collatz (int n)
     int steps = 0;
 
     if (n == 1)
-        {
-            steps =+ 1;
-            return 0;
-        }
+    {
+        steps += 1;
+        return 0;
+    }
     else if (n % 2 == 0)
-        {
-            steps =+ 2;
-            collatz(n/2);
-        }
-
-
+    {
+        steps += 2;
+        collatz(n/2);
+    }
     else
-    collatz (3*n + 1);
-}
+    {
+        steps += 3;
+        collatz (3*n + 1);
+    }
 
 int main(void)
 {
