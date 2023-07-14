bdc246df9f2485d432745304bff11f019934ce7b
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-07-14 02:08:49 +0000

    Fri, Jul 14, 2023, 4:08 AM +02:00

---
+++ structures/structures.c
@@ -0,0 +1,20 @@
+#include <cs50.h>
+#include <stdio.h>
+
+typedef struct
+{
+    string name;
+    int vote;
+}
+candidate;
+
+int main(void)
+{
+    candidate president = get_candidate("Enter a candidate: ")
+}
+
+candidate get_candidate (string, int)
+{
+    get_string = ""
+    get_vote
+}
