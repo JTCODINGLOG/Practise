d35dd7bbbd7efa185a1fc61fbd0206684b316cae
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-07-24 11:42:49 +0000

    Mon, Jul 24, 2023, 1:42 PM +02:00

--- temps/temps.c
+++ temps/temps.c
@@ -63,6 +63,7 @@ int main(void)
 void sort_cities(void)
 {
     // Add your code here
+    for (int j = 0; ;j++)
             for (int i = 0; i < NUM_CITIES - 1; i++)
             {
                 if (temps[i].temp > temps[i + 1].temp)
