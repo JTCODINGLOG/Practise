a71de64b9e42e4f7a1feb61e180b64378070dcef
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-07-26 01:57:16 +0000

    Wed, Jul 26, 2023, 3:57 AM +02:00

--- snackbar/snackbar.c
+++ snackbar/snackbar.c
@@ -44,36 +44,6 @@ float get_cost(string item);
 
 int main(void)
 {
-    menu[0].item = "Burger";
-    menu[0].price = 9.5;
-
-    menu[1].item = "Vegan Burguer";
-    menu[1].price = 11;
-
-    menu[2].item = "Hot Dog";
-    menu[2].price = 5;
-
-    menu[3].item = "Cheese Dog";
-    menu[3].price = 7;
-
-    menu[4].item = "Fries";
-    menu[4].price = 5;
-
-    menu[5].item = "Cheese Fries";
-    menu[5].price = 6;
-
-    menu[6].item = "Cold Pressed Juice";
-    menu[6].price = 7;
-
-    menu[7].item = "Cold Brew";
-    menu[7].price = 3;
-
-    menu[8].item = "Water";
-    menu[8].price = 2;
-
-    menu[9].item = "Soda";
-    menu[9].price = 2;
-
     add_items();
 
     printf("\nWelcome to Beach Burger Shack!\n");
@@ -104,11 +74,46 @@ int main(void)
 // Add at least the first four items to the menu array
 void add_items(void)
 {
+
+    menu[0].item = "Burger";
+    menu[0].price = 9.5;
+
+    menu[1].item = "Vegan Burguer";
+    menu[1].price = 11;
+
+    menu[2].item = "Hot Dog";
+    menu[2].price = 5;
+
+    menu[3].item = "Cheese Dog";
+    menu[3].price = 7;
+
+    menu[4].item = "Fries";
+    menu[4].price = 5;
+
+    menu[5].item = "Cheese Fries";
+    menu[5].price = 6;
+
+    menu[6].item = "Cold Pressed Juice";
+    menu[6].price = 7;
+
+    menu[7].item = "Cold Brew";
+    menu[7].price = 3;
+
+    menu[8].item = "Water";
+    menu[8].price = 2;
+
+    menu[9].item = "Soda";
+    menu[9].price = 2;
+
+
+
     return;
 }
 
 // Search through the menu array to find an item's cost
 float get_cost(string item)
 {
+    if (strcmp(strings[i], s) == 0)
+    
     return 0.0;
 }
