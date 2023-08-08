2fcb40e6a1b401997e527d6b7c80d08a781a7a00
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-08-08 10:58:12 +0000

    Tue, Aug 8, 2023, 12:58 PM +02:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -71,7 +71,7 @@ bool vote(string name)
         if (name == candidates[i].name)
         {
             candidates[i].votes += 1;
-            return false;
+            return true;
         }
      }
     return true;
