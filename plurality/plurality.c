2104cea80b42b1b2bfad3d3424e0bca9ca4f7c83
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-08-08 11:12:34 +0000

    Tue, Aug 8, 2023, 1:12 PM +02:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -71,10 +71,11 @@ bool vote(string name)
         if (name == candidates[i].name)
         {
             candidates[i].votes += 1;
-            return false;
+            return true;
+            break;
         }
     }
-    return true;
+    return false;
 }
 // Print the winner (or winners) of the election
 void print_winner(void)
