7c5d8f0702ce47fd26d3ce69f3cb2fbb6716a344
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-08-14 11:30:10 +0000

    Mon, Aug 14, 2023, 1:30 PM +02:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -118,7 +118,7 @@ void record_preferences(int ranks[])
     // TODO
     for (int i = 0; i < candidate_count; i++)
     {
-        preferences[]
+        preferences[0][2] = 
     }
     return;
 }
