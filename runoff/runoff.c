538909a7b3874d57d11e8cc91147f523544536d4
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-08-15 07:07:11 +0000

    Tue, Aug 15, 2023, 9:07 AM +02:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -131,9 +131,10 @@ bool vote(int voter, int rank, string name)
     // TODO
     for  (int i = 0; i < candidate_count; i++)
     {
-        if (strcmp(name, candidates) == 0)
+        if (strcmp(name, candidates[i]) == 0)
         {
-            
+            preferences[voter][rank] = i
+            return true;
         }
     }
     return false;
