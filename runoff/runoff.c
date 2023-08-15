e753747d50346f46cfb8faade490e3ccf22d2bec
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-08-15 08:26:52 +0000

    Tue, Aug 15, 2023, 10:26 AM +02:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -190,6 +190,7 @@ int find_min(void)
 bool is_tie(int min)
 {
     for (int i = 0; i < candidate_count; i++)
+    if (candidates[i].eliminated == false && candidates[]
 
     // TODO
     return false;
