3081398ee3869c9b335c3ff20c7e8c48fca30ce5
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-08-15 07:56:52 +0000

    Tue, Aug 15, 2023, 9:56 AM +02:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -180,7 +180,10 @@ int find_min(void)
     {
         if (candidates[i].votes < candidates[i + 1].votes)
         {
-            min_vote = candidates[i].votes;
+            if (candidates[i].eliminated == false)
+            {
+                min_vote = candidates[i].votes;
+            }
         }
     }
     for (int i = 0; i < candidate_count; i++)
