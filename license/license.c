8b196245a5096ebb7333d8febad51fad04bb4b57
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-09-13 10:12:28 +0000

    Wed, Sep 13, 2023, 12:12 PM +02:00

--- license/license.c
+++ license/license.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
     }
 
     // Create buffer to read into
-    char buffer[7];
+    char *buffer[7];
 
     // Create array to store plate numbers
     char *plates[8];
@@ -33,7 +33,7 @@ int main(int argc, char *argv[])
         buffer[6] = '\0';
 
         // Save plate number in array
-        *plates[idx] = buffer;
+        *plates[idx] = buffer[0];
         idx++;
     }
 
