34fe129674f21f3aeaf28c639fe2ff1c5c5e17c1
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-09-23 07:31:04 +0000

    Sat, Sep 23, 2023, 9:31 AM +02:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -2,5 +2,14 @@
 
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < width; i++)
+    {
+        for (int j = 0; j < height; j++)
+        {
+            RGBTRIPLE pixel = image[i][j];
+            if()
+        }
+    }
+
     // Change all black pixels to a color of your choosing
 }
