7ee3bb96d1482569970bd85efa3b3aa4d3e90cec
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-09-28 03:15:52 +0000

    Thu, Sep 28, 2023, 5:15 AM +02:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -10,9 +10,14 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < height; j++)
         {
-            int average = round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3);
-            printf ("%i", average);
-            return;
+            Blue
+            Green
+            Red
+
+            int average = round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3.0);
+            image[i][j].rgbtBlue = average;
+            image[i][j].rgbtGreen = average;
+            image[i][j].rgbtRed = average;
         }
     }
     return;
