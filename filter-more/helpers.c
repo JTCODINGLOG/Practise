fb5fa41f746dae011baa165648d56c6f7f458a1f
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-09-28 03:21:01 +0000

    Thu, Sep 28, 2023, 5:21 AM +02:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -5,9 +5,9 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    for (int i = 0; i < width; i++)
+    for (int i = 0; i < height; i++)
     {
-        for (int j = 0; j < height; j++)
+        for (int j = 0; j < width; j++)
         {
             int average = round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3.0);
             image[i][j].rgbtBlue = average;
