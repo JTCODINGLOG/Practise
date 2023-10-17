0d013cc04e79187645fec3b6ad27bd78e43c965b
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-10-17 11:23:57 +0000

    Tue, Oct 17, 2023, 1:23 PM +02:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -63,7 +63,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                     int y = j + l;
                     if ( x < 0 || y < 0 || x > (height -1) || y > (width -1))
                     {
-                        
+                        continue;
                     }
                     tot_red += copy[i+k][j+l].rgbtRed;
                     tot_green += copy[i+k][j+l].rgbtGreen;
