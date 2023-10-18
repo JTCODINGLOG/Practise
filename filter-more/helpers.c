6652bd10c83796dea7f21fe5ec121baa554d4010
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-10-18 12:17:44 +0000

    Wed, Oct 18, 2023, 2:17 PM +02:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -134,7 +134,9 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
                     tot_blue_gy += (gy_matrix[x_m][y_m]*copy[x][y].rgbtBlue);
                 }
             }
-            int tot_red = sqrt(tot_red_gx + tot_red_gy;
+            int tot_red = sqrt(pow(tot_red_gx, 2) + pow(tot_red_gy, 2));
+            int tot_green = sqrt(pow(tot_green_gx, 2) + pow(tot_green_gy, 2));
+            int tot_blue = sqrt(pow(tot_blue_gx, 2) + pow(tot_blue_gy, 2));
         }
     }
     return;
