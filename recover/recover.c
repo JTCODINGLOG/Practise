272b356765ffed4ad8d1922686d32538fd8596fb
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-10-20 03:20:34 +0000

    Fri, Oct 20, 2023, 5:20 AM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -2,25 +2,27 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+const int BLOCK_SIZE = 512;
+
 int main(int argc, char *argv[])
 {
-
-    // 0xff, 0xd8, oxff, oxesomething.
     //512 bytes until a new JPEG is found.
     //empty spaces are 0.
 
     typedef uint8_t BYTE;
 
-    FILE*file = fopen(argv[1], "r");
+    FILE*infile = fopen(argv[1], "r");
 
+    FILE*outfile = fopen(/*allocate room for image*/,"w");
 
-    int BLOCK_SIZE = 512;
+    BYTE buffer[BLOCK_SIZE];
 
-    BYTE buffer;
-
-    while (fread(buffer, 1, BLOCK_SIZE, file))
+    while (fread(buffer, 1, BLOCK_SIZE, infile))
     {
-
+        if (/*the four first bytes have to be 0xff, 0xd8, oxff, oxe*/)
+        {
+            fwrite(buffer, 1, BLOCK_SIZE, outfile);
+        }
     }
 
 }
