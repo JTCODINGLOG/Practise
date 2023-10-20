e953a73ae40a9319387f5aa29dd549db3f3b9fcf
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-10-20 03:08:36 +0000

    Fri, Oct 20, 2023, 5:08 AM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -6,14 +6,16 @@ int main(int argc, char *argv[])
 {
 
     // 0xff, 0xd8, oxff, oxesomething.
-    //512 blocks, empty spaces are 0.
+    //512 bytes until a new JPEG is found.
+    //empty spaces are 0.
+    
     typedef uint8_t BYTE;
 
     FILE*file = fopen(argv[1], "r");
 
 
     int BLOCK_SIZE = 512;
-    
+
     BYTE buffer;
 
     while (fread(buffer, 1, BLOCK_SIZE, argv[1]))
