a99a4120f60b4474d77ca915f7b247954b6f2e77
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-10-20 04:04:14 +0000

    Fri, Oct 20, 2023, 6:04 AM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 
     while (fread(buffer, 1, BLOCK_SIZE, infile) == BLOCK_SIZE)
     {   /*the four first bytes have to be 0xff, 0xd8, oxff, oxe*/
-        if (buffer[0] = ff && buffer[1] == d8 && buffer[2] == ff &&)
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & oxf0) == 0xe0 )
         {
             fclose(/*old JPEG file*/)
             FILE*outfile = fopen(/*allocate room for image*/,"w");
