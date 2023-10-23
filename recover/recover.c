57a30774fed12800fc7c017fb61c4ed95c52b7ff
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-10-23 08:59:14 +0000

    Mon, Oct 23, 2023, 10:59 AM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -35,7 +35,13 @@ int main(int argc, char *argv[])
             if (counter = 0)
             {
                 sprintf(counter, "03i.jpg", 2);
-                int* outfile_ptr = (int*) malloc(sizeof(BLOCK_SIZE))
+                
+                int* outfile_ptr = (int*) malloc(sizeof(BLOCK_SIZE));
+                if (outfile_ptr == NULL)
+                {
+                    printf("Memory not allocated for output file"\n);
+                    return 1;
+                }
                 FILE*outfile = fopen(/*allocate room for image*/,"w");
                 fwrite(buffer, 1, BLOCK_SIZE, outfile);
                 counter++;
