1ba9c5a38eff54bbbc7652e4505aa52e7ba6f717
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-10-24 06:13:25 +0000

    Tue, Oct 24, 2023, 8:13 AM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -33,7 +33,7 @@ int main(int argc, char *argv[])
     int counter_image = 0;
 
     while (fread(buffer, 1, BLOCK_SIZE, infile) == BLOCK_SIZE)
-    {  
+    {
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0 )
         {
             if (counter_image != 0)
@@ -53,6 +53,8 @@ int main(int argc, char *argv[])
             }
             counter_image++;
         }
+        else
+        {}
 
         fwrite(buffer, 1, BLOCK_SIZE, outfile);
     }
