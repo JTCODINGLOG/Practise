88228a32ba77dc3d7c18f02b500cae16f8a91649
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-10-24 06:42:07 +0000

    Tue, Oct 24, 2023, 8:42 AM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -51,7 +51,7 @@ int main(int argc, char *argv[])
             }
             counter_image++;
         }
-        else
+        if else (outfile = NULL)
         {
             continue;
         }
