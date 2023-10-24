360152ad21afd30c8a5edd4a45083bd94e9917fb
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-10-24 04:27:53 +0000

    Tue, Oct 24, 2023, 6:27 AM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -43,7 +43,7 @@ int main(int argc, char *argv[])
             sprintf(outfile_name, "%03i.jpg", counter_image);
             outfile = fopen(outfile_name,"w");
             counter_image++;
-
+            
             if (outfile == NULL)
             {
                 printf("Output file not correctly opened"\n);
