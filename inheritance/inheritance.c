a7be9cfea178cbd280b5d3c836ef6aeb0c5fb07d
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-11-28 09:18:05 +0000

    Tue, Nov 28, 2023, 10:18 AM +01:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -110,11 +110,10 @@ void free_family(person *p)
         if (p->parents[i] != NULL)
         {
             free_family(p->parents[i]);
-
         }
     }
     free(p);
-    
+
     // TODO: Free child
 }
 
