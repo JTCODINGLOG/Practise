ecb521925064a51801111f8fa9ea0121ce60c4fb
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-12-01 06:37:48 +0000

    Fri, Dec 1, 2023, 7:37 AM +01:00

--- readability/readability.c
+++ readability/readability.c
@@ -6,5 +6,9 @@ int main(void)
 {
     string text = get_string("Text: ");
     int lenght = strlen(text);
+    for (int i = 0; i < lenght; i++)
+    {
+        if ( text[i]
+    }
     printf ("%i\n", lenght);
 }
