bae433349bebed32b3abd8a757b0975b0ca0504a
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-12-01 06:49:12 +0000

    Fri, Dec 1, 2023, 7:49 AM +01:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,15 +1,28 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+
+int count_letters (string text);
 
 int main(void)
 {
-    
     string text = get_string("Text: ");
-    int lenght = strlen(text);
-    for (int i = 0; i < lenght; i++)
-    {
-        if ( 64 < text[i] < 91
-    }
+    printf ("%i\n", count_letters(text));
+
+
     printf ("%i\n", lenght);
+}
+
+int count_letters (string text)
+{
+    int letter_counter = 0;
+    for (int i = 0; i < strlen(text); lenght; i++)
+    {
+        if (isalpha(text[i]))
+        {
+            letter_counter++;
+        }
+    }
+    return letter_counter;
 }
