4d25fea6ce3f8b34a2139172c2958c8d88232110
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-12-01 06:33:15 +0000

    Fri, Dec 1, 2023, 7:33 AM +01:00

--- readability/readability.c
+++ readability/readability.c
@@ -4,12 +4,7 @@
 int main(void)
 {
     string text = get_string("Text: ");
-    0\
-    do
-    (
-        count
-    )
-    while (text[i][j] =! NULL)
+    strlen()
     float L = text /100
     float s
 }
