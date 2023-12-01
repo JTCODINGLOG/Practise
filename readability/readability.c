2715286ffb6ed25ce276771f8697adca159f40c6
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-12-01 07:27:56 +0000

    Fri, Dec 1, 2023, 8:27 AM +01:00

--- readability/readability.c
+++ readability/readability.c
@@ -34,7 +34,7 @@ int count_words (string text)
     int word_counter = 0;
     for (int i = 0; i <strlen(text); i++)
     {
-        if (isalpha(text[i]) && (isspace(text[i+1]) || ispunct(text[i+1])))
+        if (isalpha(text[i]) && isspace(text[i+1]) || ispunct(text[i+1]))
         {
             if (text[i+1] == '-')
             {
