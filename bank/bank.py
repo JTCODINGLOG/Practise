8a9cae091ae7981559557b0bb0ffa7663ba5be34
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-12-14 09:47:20 +0000

    Thu, Dec 14, 2023, 10:47 AM +01:00

--- bank/bank.py
+++ bank/bank.py
@@ -4,7 +4,8 @@
 
 greeting = input("Greeting: ")
 greeting.lower()
-if "hello" in greeting
-
-elif(greeting[0] == "h")
+if "hello" in greeting:
+    print("$0")
+elif(greeting[0] == "h"):
+    printf
 else(greeting[0] != "h")
