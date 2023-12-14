27e80517d4b32b37967b86d5dfbe21a3ca505f1d
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-12-14 09:48:36 +0000

    Thu, Dec 14, 2023, 10:48 AM +01:00

--- bank/bank.py
+++ bank/bank.py
@@ -4,8 +4,8 @@
 
 greeting = input("Greeting: ")
 greeting.lower()
-if "hello" in greeting:
+if "hello" in greeting[0-4]:
     print("$0")
 elif(greeting[0] == "h"):
-    printf
+    printf("$20")
 else(greeting[0] != "h")
