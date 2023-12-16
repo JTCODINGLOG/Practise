dc584fd67c8b1a1198b6ecc70890734553f24ee5
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-12-16 11:20:36 +0000

    Sat, Dec 16, 2023, 12:20 PM +01:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -15,7 +15,7 @@ if le == 1:
 
 
 elif le == 3:
-    if (sys.argv[1] == "-f" or sys.argv[1] == "--font"):
+    if sys.argv[1] in ("-f", "--font"):
         figlet.setFont(font = sys.argv[2])
     else:
         print("Write the code to print in a specific font")
