8b06d8c519cc193076ccf136efb5ca4aaf2e3908
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-12-16 10:06:26 +0000

    Sat, Dec 16, 2023, 11:06 AM +01:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,7 +1,13 @@
-from sys import argv
+pip install pyfiglet
 
-if (len(argv) != (0 or 1))
-    print("Please input 0 or)
+from sys import argv
+from figlet import Figlet
+
+if len(argv) != 0:
+    print("Please input 0 or 2 command-line arguments")
+    sys.exit(1)
+
+
 
 inp = input("Input: ")
 
