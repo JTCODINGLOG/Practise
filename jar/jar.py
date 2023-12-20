cbc062064ae5be2f1507ae73a5525576cc732aaa
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-12-20 01:19:31 +0000

    Wed, Dec 20, 2023, 2:19 AM +01:00

--- jar/jar.py
+++ jar/jar.py
@@ -1,12 +1,11 @@
 class Jar:
     def __init__(self, capacity=12):
-        if capacity < 0:
-            raise ValueError
-        return capacity
+        self.capacity = capacity
+        self.size = 0
 
 
     def __str__(self):
-        return "n"
+        return self
 
     def deposit(self, n):
         if n > capacity:
