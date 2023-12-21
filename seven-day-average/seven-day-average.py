6add4156ea5c5e3ec833a18a68aa788ce11e3a7f
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2023-12-21 03:27:18 +0000

    Thu, Dec 21, 2023, 4:27 AM +01:00

--- seven-day-average/seven-day-average.py
+++ seven-day-average/seven-day-average.py
@@ -35,8 +35,8 @@ def main():
 # TODO: Create a dictionary to store 14 most recent days of new cases by state
 def calculate(reader):
 
-    previous_cases{}
-    new_cases{}
+    previous_cases = {}
+    new_cases = {}
     for row in reader
 
 
