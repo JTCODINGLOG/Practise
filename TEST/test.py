c021746df1978d97687d82e963ab301cf35af520
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2024-01-10 09:34:38 +0000

    Wed, Jan 10, 2024, 10:34 AM +01:00

--- TEST/test.py
+++ TEST/test.py
@@ -1,12 +1,10 @@
+from cs50 import get_int
 
-n = int(input("How high?: " ))
+while True:
+    n = get_int("Height: ")
+    if n > 0:
+        break
 
 for i in range(n):
-    for j in range(n-i-1):
-        print(" ", end="")
-
-    for k in range(i+1):
-        print("#", end="")
-    print()
-
-
+    print("#")
+    
