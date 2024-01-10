336553984cfc7ff7342f3d8116bc54cba85c8cb4
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2024-01-10 09:17:28 +0000

    Wed, Jan 10, 2024, 10:17 AM +01:00

--- TEST/test.py
+++ TEST/test.py
@@ -1,20 +1,13 @@
-def quest():
-    n = int(input("How high?: " ))
-    return n
 
-def hast(n):
-    for i in range(n):
-        j = i
-        for j in range(quest()):
-            print(" ", end="")
-        k = quest()-i
-        for k in range(quest()):
-            print("#", end="")
+n = int(input("How high?: " ))
+
+for i in range(n):
+    j = i
+    for j in range(n):
+        print(" ", end="")
+    k = n-i
+    for k in range(n):
+        print("#", end="")
     print()
-
-def main():
-    hast(quest())
-
-main()
 
 
