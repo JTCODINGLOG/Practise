8f68441995349628dc6dadba311ec288a84c6d51
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2024-01-10 09:06:48 +0000

    Wed, Jan 10, 2024, 10:06 AM +01:00

--- TEST/test.py
+++ TEST/test.py
@@ -2,6 +2,7 @@ def hast():
     for i in range(quest()):
         for j-i in range(quest()):
             print(" ", end="")
+        k = quest()
         for k in range(quest()):
             print("#", end="")
     print()
