f6494e348bf288a313620447c425878129ab1605
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2024-01-17 02:42:22 +0000

    Wed, Jan 17, 2024, 3:42 AM +01:00

--- TEST/test.py
+++ TEST/test.py
@@ -1,1 +1,15 @@
-people = {}
+people = [
+    {"name": "Javier", "number": "0411251528"},
+    {"name": "David", "number": "0402040184"},
+    {"name": "John", "number": "0444444444"}
+]
+
+name = input("Name: ")
+
+for person in people:
+    if person["name"] == name
+        number = person["number"]
+        print(f"Found {number}")
+        break
+else:
+    print("Not found")
