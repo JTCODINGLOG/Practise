df8d453ea848bf27a5ec90c8c49304576a0b4cc7
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2024-01-17 01:31:53 +0000

    Wed, Jan 17, 2024, 2:31 AM +01:00

--- TEST/test.py
+++ TEST/test.py
@@ -1,11 +1,11 @@
+from cs50 import get_int
+
 while True:
-    try:
-        return n = int(input("height>: "))
-    except ValueError:
-        print("Not an integer")
+    n = get_int("height: ")
+    if n > 0:
+        break
 
 k = n
-
 for i in range(n):
     k -= 1
     print(" " * k + "#" * (i+1))
