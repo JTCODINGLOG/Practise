2f1e7fbd000af4ca003a6dbe53ec47da8a1b0420
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2024-01-17 04:08:07 +0000

    Wed, Jan 17, 2024, 5:08 AM +01:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,10 +1,12 @@
 from cs50 import get_int
-from sys import exit
+import sys
 
 while True:
     n = get_int("Height: ")
     if n > 1 or n < 9:
-        exit(0)
+        break
+        sys.exit(0)
+
 
 
 k = n
