bf7dd15c82298da4517328342068726e2b543857
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2024-01-17 01:39:19 +0000

    Wed, Jan 17, 2024, 2:39 AM +01:00

--- TEST/test.py
+++ TEST/test.py
@@ -1,11 +1,4 @@
-from cs50 import get_int
+scores = [72, 73, 33]
 
-while True:
-    n = get_int("height: ")
-    if n > 0:
-        break
-
-k = n
-for i in range(n):
-    k -= 1
-    print(" " * k + "#" * (i+1))
+average = sum(scores) / len(scores)
+print(f"Average: {average}")
