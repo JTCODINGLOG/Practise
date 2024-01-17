517677ce9099c0219355d257e2d1920a55a7325c
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2024-01-17 04:06:11 +0000

    Wed, Jan 17, 2024, 5:06 AM +01:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,12 +1,14 @@
 from cs50 import get_int
+from sys import exit
 
 while True:
     n = get_int("Height: ")
-    if n > 1 or n < 9
-        break
+    if n > 1 or n < 9:
+        exit(0)
+
 
 k = n
 for i in range(n):
     k -= 1
-    print(" " * k + "#" * i + 1)
+    print(" " * k + "#" * (i + 1))
 
