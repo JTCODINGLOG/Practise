dc818c0080fd91a8cf13b4c10fd5a4701bc2f26a
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2024-01-19 03:48:08 +0000

    Fri, Jan 19, 2024, 4:48 AM +01:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,10 +1,5 @@
 from cs50 import get_float
 
-quarters = 0.25
-dimes = 0.10
-nickels = 0.05
-pennies = 0.01
-
 while True:
     change = get_float("Change: ")
     if change > 0:
@@ -19,10 +14,10 @@ def changewith(change, coin):
         print(coins)
     return coins, change
 
-changewith(quarters)
-changewith(dimes)
-changewith(nickels)
-changewith(pennies)
+changewith(0.25)
+changewith(0.0)
+changewith(0.05)
+changewith(0.01)
 
 
 #if change % quarters == 0:
