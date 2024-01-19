191d917734b3a116527dc2e2f4c3d769abaf7f02
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2024-01-19 03:10:53 +0000

    Fri, Jan 19, 2024, 4:10 AM +01:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,10 +1,16 @@
 from cs50 import get_float
 
+quarters = 0.25
+dimes = 0.10
+nickels = 0.05
+pennies = 0.01
+
 while True
     pay = get_float("Change: ")
     if pay > 0:
         break
 
-
+if pay % quarters == 0
+    
 
 #0.25 0.10 0.05 0.01
