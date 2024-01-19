39457d576cb9b4a3e04eb6156185270bbed24627
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2024-01-19 04:25:50 +0000

    Fri, Jan 19, 2024, 5:25 AM +01:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,10 +1,18 @@
 from cs50 import get_float
 
-while True:
-    change = get_float("Change: ")
-    if change > 0:
-        break
-coins = 0
+def main():
+    while True:
+        change = get_float("Change: ")
+        if change > 0:
+            break
+    coins = 0
+    coins, change = changewith(0.25, coins, change)
+    coins, change = changewith(0.10, coins, change)
+    coins, change = changewith(0.05, coins, change)
+    coins, change = changewith(0.01, coins, change)
+    print(coins)
+
+
 def changewith(coin, coins, change):
     if change < coin:
         return coins, change
@@ -12,10 +20,9 @@ def changewith(coin, coins, change):
     change = round(change % coin, 2)
     return coins, change
 
-coins, change = changewith(0.25, coins, change)
-coins, change = changewith(0.10, coins, change)
-coins, change = changewith(0.05, coins, change)
-coins, change = changewith(0.01, coins, change)
-print(coins)
+
+if __name__ == "__main__":
+    main()
+
 
 
