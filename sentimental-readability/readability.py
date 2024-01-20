ddc1a831165f41b777cef4583fbd84ec346b5dfa
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2024-01-20 09:58:22 +0000

    Sat, Jan 20, 2024, 10:58 AM +01:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,23 +1,30 @@
-from nltk.tokenize import sent_tokenize, word_tokenize
 from cs50 import get_string
-import nltk
 
 
-text = get_string("Text: ")
-sentences = sent_tokenize(text)
-words = [word for word in word_tokenize(text) if word.isalpha()]
+def main():
+    text = get_string("Text: ")
 
-num_sentences = len(sentences)
-num_words = len(words)
-num_letters = sum(len(word) for word in words)
+    L = c_letters(text) / c_words(text) * 100
+    S = c_sentences(text) / c_words(text) * 100
+    index = round(0.0588 * L - 0.296 * S - 15.8)
 
-L = num_letters / num_words * 100
-S = num_sentences / num_words * 100
-index = round(0.0588 * L - 0.296 * S - 15.8)
+    if index < 1:
+        return print("Before grade 1\n")
+    elif index > 16:
+        return print("Grade 16+\n")
+    else:
+        return print(f"Grade {index}")
 
-if index < 1:
-    print("Before Grade 1\n")
-elif index > 16:
-    print("Grade 16+\n")
-else:
-    print(f"Grade {index}")
+def c_letters(txt):
+
+
+
+def c_words(txt):
+
+
+
+def c_sentences(txt):
+
+if __name__ == "__main__":
+    main()
+
