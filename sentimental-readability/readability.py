10cca09fa9a001a1e75c5518bcdeda56035c5710
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2024-01-20 03:53:18 +0000

    Sat, Jan 20, 2024, 4:53 AM +01:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -5,3 +5,11 @@ text = get_string("Text: ")
 L = c_letters(text) / c_words(text) * 100
 S = c_sentences(text) / c_words(text) * 100
 index = round(0.0588 * L - 0.296 * S - 15.8)
+
+if index < 1:
+    return print("Before grade 1\n")
+elif index > 16:
+    return print("Grade 16+\n")
+else:
+    return print(f"Grade {index}")  
+
