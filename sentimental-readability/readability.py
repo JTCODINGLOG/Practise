892942415bb86e95e9343f7ea560a6b9be2a57d9
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2024-01-20 03:48:00 +0000

    Sat, Jan 20, 2024, 4:48 AM +01:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -2,3 +2,6 @@ from cs50 import get_string
 
 text = get_string("Text: ")
 
+L = c_letters(text) / c_words(text) * 100
+S = c_sentences(text) / c_words(text) * 100
+index = round(0.0588 * L - 0.296 * S - 15.8)
