b976d989c2c48b123bbfd395ee013a69b1ad95b7
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2024-01-22 05:13:57 +0000

    Mon, Jan 22, 2024, 6:13 AM +01:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -5,9 +5,10 @@ import string
 def main():
     text = get_string("Text: ")
 
-    L = c_letters(text) / c_words(text) * 100
-    S = c_sentences(text) / c_words(text) * 100
-    index = round(0.0588 * L - 0.296 * S - 15.8)
+    print(c_letters(text))
+    #L = c_letters(text) / c_words(text) * 100
+    #S = c_sentences(text) / c_words(text) * 100
+    #index = round(0.0588 * L - 0.296 * S - 15.8)
 
     if index < 1:
         return print("Before Grade 1\n")
