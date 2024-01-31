f6e6c1ac21c433548b4abe1c10db9bf21320c62c
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2024-01-31 04:01:20 +0000

    Wed, Jan 31, 2024, 5:01 AM +01:00

--- dna/dna.py
+++ dna/dna.py
@@ -14,7 +14,9 @@ def main():
         databasedna = csv.DictReader(file)
 
     # TODO: Read DNA sequence file into a variable
-
+    with open(sys.argv[2]) as file:
+        dnasequence = file.read()
+        print(dnasequence)
     # TODO: Find longest match of each STR in DNA sequence
 
     # TODO: Check database for matching profiles
