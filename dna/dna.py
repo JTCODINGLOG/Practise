f677b3d71fe5e2fee791ccdaf0c33de7d280956c
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2024-02-01 04:57:00 +0000

    Thu, Feb 1, 2024, 5:57 AM +01:00

--- dna/dna.py
+++ dna/dna.py
@@ -18,7 +18,7 @@ def main():
         dnasequence = file.read()
 
     # TODO: Find longest match of each STR in DNA sequence
-    print(databasedna.fieldnames())
+    print(databasedna.fieldnames)
     # TODO: Check database for matching profiles
 
     return
