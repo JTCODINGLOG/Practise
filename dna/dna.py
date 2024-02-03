708d3789974f983697d0f8e417f25df1dd740941
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2024-02-03 09:15:54 +0000

    Sat, Feb 3, 2024, 10:15 AM +01:00

--- dna/dna.py
+++ dna/dna.py
@@ -25,6 +25,7 @@ def main():
     dictruns = {}
     for i in range(1, len(csvhead)-1):
         dictruns[csvhead[i]] = str(longest_match(dnasequence, csvhead[i]))
+        
     print(csvhead[1:0])
     #print("-----------------------------------")
     print(dictruns)
