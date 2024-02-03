b17f8f7b3ebcb6f5e97d8a80e290e4a10083d324
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2024-02-03 09:19:17 +0000

    Sat, Feb 3, 2024, 10:19 AM +01:00

--- dna/dna.py
+++ dna/dna.py
@@ -6,7 +6,7 @@ def main():
 
     # TODO: Check for command-line usage
     if len(sys.argv) != 3:
-        print("Please input 2 ccommand-line arguments: first is a CSV file, second is a text file")
+        print("Please input 2 command-line arguments: first is a CSV file, second is a text file")
         sys.exit(1)
 
     # TODO: Read database file into a variable
@@ -25,7 +25,7 @@ def main():
     dictruns = {}
     for i in range(1, len(csvhead)-1):
         dictruns[csvhead[i]] = str(longest_match(dnasequence, csvhead[i]))
-        
+
     print(csvhead[1:0])
     #print("-----------------------------------")
     print(dictruns)
