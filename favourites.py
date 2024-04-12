601673c6150cd713cd7dd56cd65b95d68864d1a2
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2024-04-12 05:03:30 +0000

    Fri, Apr 12, 2024, 7:03 AM +02:00

--- favourites.py
+++ favourites.py
@@ -4,4 +4,7 @@ db = SQL("sqlite:///favorites.db")
 
 favorite = input("Favorite: ")
 
-db.execute("SELECT COUNT(*) AS n FROM favorites WHERE problem = ?")
+rows = db.execute("SELECT COUNT(*) AS n FROM favorites WHERE problem = ?", favorite)
+row = rows[0]
+
+print(row["n"])
