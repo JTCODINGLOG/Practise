2bb173b63f1cc581c8dc2889bacf903505a062d8
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2024-04-18 13:13:42 +0000

    Thu, Apr 18, 2024, 3:13 PM +02:00

--- movies/6.sql
+++ movies/6.sql
@@ -2,5 +2,6 @@ SELECT AVG(rating) FROM ratings WHERE movie_id IN (
     SELECT id FROM movies WHERE year= 2012);
 
 SELECT AVG(rating) FROM ratings
-    JOIN movies ON ratings.movie_id = movies.id;
+    JOIN movies ON ratings.movie_id = movies.id
+        WHERE year = 2012;
 
