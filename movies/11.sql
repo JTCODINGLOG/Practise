3d7041cc72d2910f4d88fc9e97d2a031da35d095
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2024-04-26 13:15:36 +0000

    Fri, Apr 26, 2024, 3:15 PM +02:00

--- movies/11.sql
+++ movies/11.sql
@@ -1,6 +1,6 @@
 SELECT title FROM movies
     JOIN stars ON movies.id = stars.movie_id
     JOIN ratings ON movies.id = ratings.movie_id
-    JOIN people ON 
+    JOIN people ON movies.
         WHERE name = 'Chadwick Boseman'
             ORDER BY ratings DESC LIMIT 5;
