2dee9399bfd92172d23e69219ce83a3347d5aa1a
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2024-04-26 12:27:07 +0000

    Fri, Apr 26, 2024, 2:27 PM +02:00

--- movies/7.sql
+++ movies/7.sql
@@ -1,3 +1,4 @@
 SELECT title, rating FROM ratings
     JOIN movies ON ratings.movie_id = movies.id
-        WHERE year = 2010 WHERE rating IS NOT NULL ORDER BY rating DESC, title;
+        WHERE year = 2010 AND rating IS NOT NULL
+            ORDER BY rating DESC, title;
