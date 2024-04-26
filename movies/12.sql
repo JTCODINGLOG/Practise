48d80ba4dd62301eedb50d13274a13b6569bb732
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2024-04-26 13:36:44 +0000

    Fri, Apr 26, 2024, 3:36 PM +02:00

--- movies/12.sql
+++ movies/12.sql
@@ -2,4 +2,5 @@ SELECT title FROM movies
     JOIN stars ON movies.id = stars.movie_id
     JOIN people ON stars.person_id = people.id
         CASE
-            WHEN
+            WHEN (SELECT id FROM people WHERE name = 'Bradley Cooper') =
+                 (SELECT id FROM people WHERE name = 'Jennifer Lawrence')
