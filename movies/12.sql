fd97757e96f7d64bb842da2fe1e97cfffae08a1e
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2024-04-29 03:47:58 +0000

    Mon, Apr 29, 2024, 5:47 AM +02:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,4 +1,4 @@
-SELECT counts(*) FROM movies
+SELECT count(*) FROM movies
     JOIN stars ON movies.id = stars.movie_id
     JOIN people ON stars.person_id = people.id
         WHERE people.name IN ('Bradley Cooper', 'Jennifer Lawrence')
