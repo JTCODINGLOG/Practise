14c7e9a24edc3032e1ae441ba09f9d4cba17223b
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2024-04-29 03:59:32 +0000

    Mon, Apr 29, 2024, 5:59 AM +02:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,4 +1,4 @@
-SELECT name FROM people
+SELECT id FROM people
     JOIN stars ON people.id = stars.person_id
     JOIN movies ON stars.movie_id = movies.id
-        WHERE 
+        WHERE people.name = 'Kevin Bacon' AND people.birth = 1958
