79e038920e4d0487b330a61c10b7261beef1950b
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2024-04-29 04:17:48 +0000

    Mon, Apr 29, 2024, 6:17 AM +02:00

--- movies/13.sql
+++ movies/13.sql
@@ -3,7 +3,7 @@ SELECT id FROM people
     JOIN movies ON stars.movie_id = movies.id
         WHERE people.name = 'Kevin Bacon' AND people.birth = 1958
 
-SELECT id FROM movies WHERE movie_id
+SELECT person_id FROM stars WHERE 
 SELECT movie_id FROM stars WHERE person_id IN(
     SELECT id FROM people WHERE people.name = 'Kevin Bacon' AND people.birth = 1958)
 
