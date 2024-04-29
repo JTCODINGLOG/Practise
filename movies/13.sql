26b4f01f169483fe11b71fa38f4984dbf3a240df
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2024-04-29 04:31:24 +0000

    Mon, Apr 29, 2024, 6:31 AM +02:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,3 +1,18 @@
+/*FROM people
+JOIN stars ON people.id = stars.person_id
+JOIN movies ON stars.movie_id = movies.id
+WHERE people.name != 'Kevin Bacon'
+AND movies.id IN (
+    SELECT movie_id
+    FROM stars
+    WHERE person_id = (
+        SELECT id
+        FROM people
+        WHERE name = 'Kevin Bacon' AND birth = 1958
+    )
+)*/
+
+
 SELECT COUNT(name) FROM people WHERE id IN (
     SELECT person_id FROM stars WHERE movie_id IN(
         SELECT movie_id FROM stars WHERE person_id IN(
