fbac047e2c1c9a4b0103e223c099337b15a5dc03
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2024-04-30 09:35:19 +0000

    Tue, Apr 30, 2024, 11:35 AM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -9,4 +9,4 @@ now that we know the specific time (10:15am) and that the 3 witnesses where
 interviewed the same day, we can have a look into the interviews and maybe later the bakery security logs
 (we also know that all of them mentioned the bakery)*/
 SELECT transcript FROM interviews
-WHERE
+WHERE year = 2023 AND month = 7 AND day = 28;
