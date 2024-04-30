50539b6800842b152751f0d6b5d33fa9f1f3612f
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2024-04-30 09:45:44 +0000

    Tue, Apr 30, 2024, 11:45 AM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -7,6 +7,12 @@ AND street = 'Humphrey Street';
 
 now that we know the specific time (10:15am) and that the 3 witnesses where
 interviewed the same day, we can have a look into the interviews and maybe later the bakery security logs
-(we also know that all of them mentioned the bakery)*/
+(we also know that all of them mentioned the bakery)
 SELECT transcript FROM interviews
 WHERE year = 2023 AND month = 7 AND day = 28;
+
+Let's check bakery security logs*/
+SELECT activity, license_plate FROM bakery_security_logs
+WHERE year = 2023 AND month = 7 AND day = 28
+AND hour = 10 AND minute
+
