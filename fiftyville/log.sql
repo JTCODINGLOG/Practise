d296871babba4ee1fd3fe82010505ef99b78ae45
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2024-04-30 09:33:49 +0000

    Tue, Apr 30, 2024, 11:33 AM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,2 +1,12 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+/*to know more details about the crime we have search its report
+SELECT description FROM crime_scene_reports
+WHERE year = 2023 AND month = 7 AND day = 28
+AND street = 'Humphrey Street';
+
+now that we know the specific time (10:15am) and that the 3 witnesses where
+interviewed the same day, we can have a look into the interviews and maybe later the bakery security logs
+(we also know that all of them mentioned the bakery)*/
+SELECT transcript FROM interviews
+WHERE
