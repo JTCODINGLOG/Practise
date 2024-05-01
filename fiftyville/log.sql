79b73faacdee86a68336c3646169150d619b24ee
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2024-05-01 12:10:34 +0000

    Wed, May 1, 2024, 2:10 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -82,12 +82,11 @@ SELECT city FROM airports WHERE id = (
 Lets try to guess the name:*/
 
 SELECT name, passport_number FROM people WHERE phone_number IN
-(SELECT caller FROM phone_calls
-WHERE year = 2023 AND month = 7 AND day = 28 AND
-duration < 60) AND license_plate IN
-(SELECT license_plate FROM bakery_security_logs
-WHERE year = 2023 AND month = 7 AND day = 28
-AND hour = 10 AND minute BETWEEN 15 AND 25);
+    (SELECT caller FROM phone_calls WHERE year = 2023 AND month = 7 AND day = 28 AND duration < 60)
+    AND license_plate IN
+    (SELECT license_plate FROM bakery_security_logs WHERE year = 2023 AND month = 7 AND day = 28 AND hour = 10 AND minute BETWEEN 15 AND 25)
+    AND passport_number IN;
+
 
 
 
