4fa1096251448fb7436fc52c021aadeb33fcf945
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2024-05-01 11:49:14 +0000

    Wed, May 1, 2024, 1:49 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -68,7 +68,7 @@ Bruce, 5773159633
 Let's try now to connect the last results to the flights*/
 SELECT id FROM flights WHERE origin_airport_id =(
     SELECT id FROM airports WHERE city = 'Fiftyville')
-AND year = 2023 AND month = 7 AND day = 29;
+AND year = 2023 AND month = 7 AND day = 29 ORDER BY hour ASC LIMIT 1;
 
 
 
