3c8394fc75af924047f3e8303ce21e059804a565
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2024-05-01 09:54:15 +0000

    Wed, May 1, 2024, 11:54 AM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -7,18 +7,23 @@ AND street = 'Humphrey Street';
 
 now that we know the specific time (10:15am) and that the 3 witnesses where
 interviewed the same day, we can have a look into the interviews and maybe later the bakery security logs
-(we also know that all of them mentioned the bakery)*/
+(we also know that all of them mentioned the bakery)
 SELECT transcript FROM interviews
 WHERE year = 2023 AND month = 7 AND day = 28;
-/*
+
 Let's check bakery security logs
 SELECT activity, license_plate FROM bakery_security_logs
 WHERE year = 2023 AND month = 7 AND day = 28
 AND hour = 10 AND minute BETWEEN 15 AND 25;
-
 It gives me 8 license_plates:
 activity:exit
 license_plate: 5P2BI95, 94KL13X, 6P58WS2, 4328GD8
-            G412CB7, L93JTIZ, 322W7JE, 0NTHK55/*
+            G412CB7, L93JTIZ, 322W7JE, 0NTHK55
+
+Let's check atm transactions*/
+SELECT account_number, 
+
+
+
 
 
