8bf76bdc5bcaacf18296f246327e94e23f5cbd78
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2024-07-27 07:09:23 +0000

    Sat, Jul 27, 2024, 9:09 AM +02:00

--- TEST/Kaplan.py
+++ TEST/Kaplan.py
@@ -2,5 +2,7 @@ age = int(input("Enter your age:"))
 
 if age < 18:
     print("Sorry under 18s are not allowed")
+elif age == 18:
+    print("We will invite you to your first drink, chuck it!")
 else:
     print("Welcome and drink!")
