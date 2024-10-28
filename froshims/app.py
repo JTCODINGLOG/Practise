40949c05e8a7642acf468a026ea34ae4d4bef652
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2024-10-28 20:27:53 +0000

    Tue, Oct 29, 2024, 4:27 AM +08:00

--- froshims/app.py
+++ froshims/app.py
@@ -12,14 +12,22 @@ def index():
 
 @app.route("/register", methods=["POST"])
 def register():
-    #this is when using checkbox or radio buttons:
-
+    # This is when using checkbox or radio buttons:
+    # Validate name
     if not request.form.get("name"):
-        return render_template("failure.html")
+        return render_template("error.html", message="Missing name")
+
+    # Validate sport
+    if not sport:
+        return render_template("error.html", message="Missing sport")
+    if sport not in SPORTS:
+        return render_template("error.html", message=""Invalid sport")
+                               
     for sport in request.form.getlist("sport"):
         if sport not in SPORTS:
             return render_template("failure.html")
-    #thi is when using dropdown:
+
+    # This when using dropdown:
     """if not request.form.get("name") or request.form.get("sport") not in SPORTS:
         return render_template("failure.html")
     """
