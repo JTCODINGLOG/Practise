158039efe0e51ab8cfe5e67c2794074d0cb64292
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2024-10-29 18:21:18 +0000

    Wed, Oct 30, 2024, 2:21 AM +08:00

--- login/app.py
+++ login/app.py
@@ -10,4 +10,8 @@ Session(app)
 def index()
     return render_template("index.html")
 
-@app.route
+@app.route("/login")
+def login():
+    if request.method == "POST":
+        session
+    return render_template("login.html")
