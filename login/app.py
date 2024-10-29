13f619ac27b567f22af3710cadb0688d81b8efc8
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2024-10-29 18:11:28 +0000

    Wed, Oct 30, 2024, 2:11 AM +08:00

--- login/app.py
+++ login/app.py
@@ -2,7 +2,9 @@ from flask import Flask, redirect, render_template, request, session
 
 app = Flask(__name__)
 
-app.config["SESSION]
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 @app.route("/")
 def index()
