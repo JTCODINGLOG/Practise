da99d6c98d78d65d440e54f0f58fd3d52535777e
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2024-11-12 08:47:25 +0000

    Tue, Nov 12, 2024, 4:47 PM +08:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -29,7 +29,7 @@ def index():
         month = request.form.get("month")
         day = request.form.get("day")
 
-        db.execute("INSERT")
+        db.execute("SELECT * FROM birthdays")
         return redirect("/")
 
     else:
