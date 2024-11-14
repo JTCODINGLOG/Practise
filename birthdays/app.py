c266aa12fe7cd402ffb36cf1083b7c2b26154705
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2024-11-14 09:29:12 +0000

    Thu, Nov 14, 2024, 5:29 PM +08:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -39,7 +39,8 @@ def index():
     else:
 
         # TODO: Display the entries in the database on index.html
-
+        rows = db.execute("SELECT * FROM birthdays")
+        
         return render_template("index.html")
 
 
