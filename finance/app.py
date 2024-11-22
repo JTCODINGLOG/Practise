12838f7dc80e1caebaad28db3aeb6b27a9ada6e4
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2024-11-22 03:32:38 +0000

    Fri, Nov 22, 2024, 11:32 AM +08:00

--- finance/app.py
+++ finance/app.py
@@ -180,7 +180,7 @@ def register():
 
         # Ensure username was submitted
         if not username:
-            return apology("must provide username", 403)
+            return apology("must provide username", 400)
 
         # Ensure password was submitted
         elif not password:
