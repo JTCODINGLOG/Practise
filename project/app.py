3a608cc780c91ba0a679c9daf18bf063959ccdbb
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2024-11-26 08:43:23 +0000

    Tue, Nov 26, 2024, 4:43 PM +08:00

--- project/app.py
+++ project/app.py
@@ -3,7 +3,7 @@ from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
-#from helpers import apology, login required, lookup, usd
+from assistants import login_required
 
 # Configure application
 app = Flask(__name__)
@@ -14,7 +14,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure databse library for SQL
-db = SQL("sqlite://project.db")
+db = SQL("sqlite:///project.db")
 
 @app.after_request
 def after_request(response):
