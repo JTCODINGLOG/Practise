99f4e2076926f511a01842f30bad792e08a8300e
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2024-12-05 04:34:50 +0000

    Thu, Dec 5, 2024, 12:34 PM +08:00

--- project/app.py
+++ project/app.py
@@ -104,8 +104,12 @@ def register():
             error = "Password must have a minimum of 8 characters and a maximum of 20"
             return render_template("register.html", error=error)
 
-        elif for char in password
-            char
+        elif not char.isupper() for char in password:
+            error = "Password must have at least one upper character"
+            return render_template("register.html", error=error)
+
+
+
             return render_template("register.html", error=error)
 
         # Ensure confirmation was submitted
