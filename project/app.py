9760ae542c8bff6f0a58a8061b729eff9c0ae6cd
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2024-12-07 08:20:14 +0000

    Sat, Dec 7, 2024, 4:20 PM +08:00

--- project/app.py
+++ project/app.py
@@ -89,11 +89,20 @@ def login():
         #Redirect to email sender
         return redirect(url_for("send", email=email))
 
-
-
     # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("login.html")
+
+    
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
 
 
 @app.route("/send", methods=["GET","POST"])
