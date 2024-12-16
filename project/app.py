f325ec8b894eae2ffe833178bf7b64ac78f98486
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2024-12-16 06:48:42 +0000

    Mon, Dec 16, 2024, 2:48 PM +08:00

--- project/app.py
+++ project/app.py
@@ -179,6 +179,8 @@ def remember_password():
             else:
                 error = "Invalid email"
                 return render_template("/remember_password.html", step=1, error=error)
+        if step == "2":
+            
 
         # When the second form of the HTML is submitted this part of code below should execute
 
