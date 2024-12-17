7a77dfa32311171a711fb1821069871213109686
JTCODINGLOG <123626201+JTCODINGLOG@users.noreply.github.com>
2024-12-17 06:53:21 +0000

    Tue, Dec 17, 2024, 2:53 PM +08:00

--- project/assistants.py
+++ project/assistants.py
@@ -36,3 +36,20 @@ def validate_email(email):
             return True
     except (NoAnswer, NXDOMAIN, Exception):
         return False
+
+# Levenshtein distance function without len=0 condition
+def levenshtein_distance(s1, s2):
+    if len(s1) < len(s2):
+        return levenshtein_distance(s2, s1)
+
+    previous_row = range(len(s2) + 1)
+    for i, c1 in enumerate(s1):
+        current_row = [i + 1]
+        for j, c2 in enumerate(s2):
+            insertions = previous_row[j + 1] + 1
+            deletions = current_row[j] + 1
+            substitutions = previous_row[j] + (c1 != c2)
+            current_row.append(min(insertions, deletions, substitutions))
+        previous_row = current_row
+
+    return previous_row[-1]
